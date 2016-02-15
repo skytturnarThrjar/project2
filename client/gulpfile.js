03b3775fcbf59ec1fdd40e5c6b50935ca585212d
Laufey Rut Guðmundsdóttir <Laufey@Laufeys-MacBook-Pro.local>
2016-02-15 11:06:02 +0000

    ddsf

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,13 +1,26 @@
 var gulp = require('gulp'),
    uglify = require('gulp-uglify'),
    concat = require('gulp-concat');
+  //  jshint = require('gulp-jshint');
+
+  // define the default task and add the watch task to it
+gulp.task('default', ['watch']);
 
 gulp.task('minify', function () {
    gulp.src('src/**/*.js')
-		// .pipe(jshint())
-		//    .pipe(jshint.reporter('default'))
       .pipe(uglify())
       .pipe(concat('app.min.js'))
       .pipe(gulp.dest('build'))
+});
 
+gulp.task('jshint', function() {
+  gulp.src('src/**/*.js')
+    .pipe(jshint())
+    .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.js', ['minify']);
+
+  //gulp.watch('src/**/*.js', ['jshint']);
 });
