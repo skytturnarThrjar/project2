2e30087c59cb46d9431d933f11adc1da909e6560
Elin <elirhau@gmail.com>
2016-02-15 13:22:38 +0000

    glup run

--- client/gulpfile.js
+++ client/gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require('gulp'),
-   uglify = require('gulp-uglify'),
-   concat = require('gulp-concat');
+    connect = require('gulp-connect'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat');
   //  jshint = require('gulp-jshint');
-
   // define the default task and add the watch task to it
-gulp.task('default', ['watch']);
+gulp.task('connect', function() {
+  connect.server({
+    port: 8000
+  });
+});
+gulp.task('default', ['watch', 'connect']);
 
 gulp.task('minify', function () {
    gulp.src('src/**/*.js')
       .pipe(uglify())
       .pipe(concat('app.min.js'))
-      .pipe(gulp.dest('build'))
+      .pipe(gulp.dest('build'));
 });
 
 gulp.task('jshint', function() {
