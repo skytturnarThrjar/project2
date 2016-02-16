d98633c949b39816f27bd391dd5b9adec502b48a
Laufey Rut Guðmundsdóttir <Laufey@lenovo-pc.hir.is>
2016-02-16 12:51:34 +0000

    YEEEE

--- client/src/room/RoomController.js
+++ client/src/room/RoomController.js
@@ -5,6 +5,31 @@ function RoomController($scope, $routeParams) {
   var id = $routeParams.id;
 }*/
 
-angular.module('chatApp').controller('RoomController', ["$scope", function ($scope, $routeParams) {
-  $scope.blabla = $routeParams.roomID;
+angular.module('chatApp').controller('RoomController', ["$scope","socket","$routeParams","$location",
+function ($scope, socket, $routeParams,$location) {
+$scope.message = "";
+$scope.roomName = $routeParams.roomID;
+
+
+$scope.sendmsg = function() {
+     socket.emit('sendmsg',{roomName: $scope.roomName, msg:$scope.message} );
+    //  console.log("her!");
+}
+
+socket.on('updatechat', function(room,chat) {
+  // console.log('Chat', chat);
+  $scope.messageHistory = chat;
+  $scope.roomName = room;
+});
+
+socket.on('userlist', function(users) {
+  // console.log(users);
+  $scope.roomlist = users;
+});
+socket.emit("users");
+
+$scope.logout = function() {
+  socket.emit('disconnect');
+  $location.path('/login');
+}
 }]);
