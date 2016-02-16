d145e336011e9036be07e9dccc488c03ed290327
vala14 <vala14@ru.is>
2016-02-16 09:35:32 +0000

    hella næs

--- client/src/login/LoginController.js
+++ client/src/login/LoginController.js
@@ -1,36 +1,21 @@
 "use strict";
 
+
 //ng Annotate svo að það se hægt að minifya koðann
-angular.module("chatApp").controller("LoginController", ["$scope",
-function ($scope,socket) {
+angular.module("chatApp").controller("LoginController", ["$scope", "socket", "$location", "$rootScope", "$routeParams",
+function ($scope, socket, $location, $rootScope, $routeParams) {
   //hægt að gera þetta svona en ekki mælt með því (eins og placeholder held ég?)
   /*$scope.login = "vala";
   $scope.pass = "flottlykilorð";*/
   $scope.username = "";
   $scope.errorMessage = "";
   $scope.login = function(){
-        socket.emit("adduser", "lubs", function(available){
-          if (available){
-              // The "dabs" username is not taken!
-          }
-  			};
-
-  //
-  // $scope.Login = function Login () {
-  // //  var socket = io.connect('http://localhost:8080');
-  //     socket.emit("adduser", $scope.user, function(available){
-  //       $scope.available = available;
-  //       console.log("works");
-  //
-  //         if (available){
-  //             // The "dabs" username is not taken!
-  //             $location("#/roomlist");
-  //             console.log("works");
-  //         }
-  //         else {
-  //           $scope.errorMessage = "Innskráning misstókst";
-  //         }
-  //         });
-  // };
-
+    socket.emit('adduser', 'vala', function (available) {
+      if (available) {
+        //$location.path('/roomlist/');
+      } else {
+        $scope.errorMessage = 'Nick is taken!';
+      }
+    });
+  };
 }]);
