abd48809239e1708ccea83b40c9832a5ea88aebf
vala14 <vala14@ru.is>
2016-02-16 11:43:53 +0000

    move to chatroom

--- client/src/roomlist/RoomlistController.js
+++ client/src/roomlist/RoomlistController.js
@@ -1,7 +1,21 @@
 // angular.module("chatApp").controller("RoomlistController", ["$scope".function($scope) {
-angular.module('chatApp').controller('RoomlistController', ["$scope", "socket", function ($scope, socket) {
+angular.module('chatApp').controller('RoomlistController', ["$scope", "socket", "$location", function ($scope, socket, $location) {
+
   socket.on("roomlist", function(rooms) {
     $scope.roomlist = rooms;
   });
   socket.emit("rooms");
+
+  $scope.errorMessage = '';
+
+  $scope.moveToRoom = function(name){
+    socket.emit('joinroom', {'room': name}, function (available) {
+      if (available) {
+        $location.path('/room/' + name);
+      }
+      else {
+        $scope.errorMessage = 'Cannot join room';
+      }
+    });
+  };
 }]);
